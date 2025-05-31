4d7901e7a149a6535c5d65eedfd47ea5e475cf3d
SAISHIVASHANKAR <awssaishivashankar@gmail.com>
2025-06-01 03:54:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-# app.py
-from flask import Flask, render_template
-from utils import load_signals
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/stat")
-def stat():
-    signal_data = load_signals()
-    return render_template("grid.html", signals=signal_data)
+@app.route("/")
+def home():
+    return "Flask is working!"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
