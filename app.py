811e27edb8a72292cfd0cdcd7e927cefc7ff7dc9
SAISHIVASHANKAR <awssaishivashankar@gmail.com>
2025-06-12 02:34:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,8 @@
-from flask import Flask, render_template
-from utils import load_signals
+from flask import Flask
+from indicator_grid import indicator_grid  # must be defined in a separate file
 
 app = Flask(__name__)
-
-@app.route("/stat")
-def indicator_grid():
-    data = load_signals()
-
-    # Compute score if not present
-    for stock in data:
-        if stock['score'] is None:
-            stock['score'] = (
-                stock.get('trend', 0) +
-                stock.get('momentum', 0) +
-                stock.get('volume', 0) +
-                stock.get('volatility', 0) +
-                stock.get('support_resistance', 0)
-            )
-
-    return render_template("indicator_grid.html", data=data)
+app.register_blueprint(indicator_grid)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
